b073a3aea978488df371d272177930e0605bd6b1
aashishdhawan <aashishdhawan@gmail.com>
2022-03-14 20:20:52 +0530

    sampletestcases

---
+++ Component/Camera.js
@@ -0,0 +1,36 @@
+// import * as React from 'react';
+// import {
+//     Text
+//     , View
+//     , StyleSheet
+//     , Image
+// } from 'react-native';
+// export default function Camera() {
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.paragraph}>
+//                 To edit the files simply drang and drop them into the editor.
+//             </Text>
+
+//         </View>
+//     );
+// }
+// const styles = StyleSheet.create({
+//     container: {
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         padding: 30,
+//         backgroundColor: "#e47eed",
+//     },
+//     paragraph: {
+//         margin: 28,
+//         marginTop: 0,
+//         fontSize: 18,
+//         fontWeight: 'bold',
+//         textAlign: 'center',
+//     },
+//     logo: {
+//         height: 128,
+//         width: 128,
+//     }
+// });
